d9a38ce3a5d23d002a9842cee491866c82c59361
Rafał Górecki <252326@edu.p.lodz.pl>
2023-11-19 13:17:40 +0100

    Mnożenie

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 
 int Policz(int a, int b){
+<<<<<<< HEAD
     return a + b;
 }
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "W pierwszym branchu; 2 + 3 =" << Policz(2, 3) <<std::endl;
+=======
+    return a * b;
+}
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "W pierwszym branchu; 2 * 3 =" << Policz(2, 3) <<std::endl;
+>>>>>>> 02
     return 0;
 }
